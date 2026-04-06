96eee4429a964b62e385f23ddc3d5d2bfee88497
gnrcrypto <gnrcrypto@gmail.com>
2026-04-05 19:48:33 -0700

    Initial exploit import

---
+++ add-to-github.sh
@@ -0,0 +1,4 @@
+git add .
+git commit -m "Initial exploit import"
+cat /home/uncle-nickypoo/github-token
+git push -u origin main
---
+++ runme.sh
@@ -0,0 +1,4 @@
+wget https://github.com/gnrcrypto/test/archive/refs/heads/main.tar.gz
+tar -xf *.gz
+cd test-main
+rm exploit137 exploit129 exploit128 exploit118 exploit116
